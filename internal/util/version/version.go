b6fe8b8f08045d2d3031f3c6c5b344cfb788bfeb
huhouhua <huhouhuam@outlook.com>
2025-06-24 23:33:38 +0800

    feat: add print version cmd

---
+++ internal/util/version/version.go
@@ -0,0 +1,68 @@
+// Copyright 2025 The Toodofun Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http:www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package version supplies version information collected at build time to
+// apimachinery components.
+package version
+
+import (
+	"encoding/json"
+	"fmt"
+	"runtime"
+)
+
+var (
+	// GitVersion is semantic version.
+	GitVersion = "v0.0.0-master+$Format:%h$"
+	// BuildDate in ISO8601 format, output of $(date -u +'%Y-%m-%dT%H:%M:%SZ').
+	BuildDate = "1970-01-01T00:00:00Z"
+	// GitCommit sha1 from git, output of $(git rev-parse HEAD).
+	GitCommit = "$Format:%H$"
+	// GitTreeState state of git tree, either "clean" or "dirty".
+	GitTreeState = ""
+)
+
+// Info contains versioning information.
+type Info struct {
+	GitVersion   string `json:"gitVersion"`
+	GitCommit    string `json:"gitCommit"`
+	GitTreeState string `json:"gitTreeState"`
+	BuildDate    string `json:"buildDate"`
+	GoVersion    string `json:"goVersion"`
+	Compiler     string `json:"compiler"`
+	Platform     string `json:"platform"`
+}
+
+// ToJSON returns the JSON string of version information.
+func (info Info) ToJSON() string {
+	s, _ := json.Marshal(info)
+
+	return string(s)
+}
+
+// Get returns the overall codebase version. It's for detecting
+// what code a binary was built from.
+func Get() Info {
+	// These variables typically come from -ldflags settings and in
+	// their absence fallback to the settings in pkg/version/base.go
+	return Info{
+		GitVersion:   GitVersion,
+		GitCommit:    GitCommit,
+		GitTreeState: GitTreeState,
+		BuildDate:    BuildDate,
+		GoVersion:    runtime.Version(),
+		Compiler:     runtime.Compiler,
+		Platform:     fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH),
+	}
+}
