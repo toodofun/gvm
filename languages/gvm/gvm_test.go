ce9618e3ab1d9ade2bd4e1fc0aa05b56b2aaa927
Rui Ma <535657150@qq.com>
2025-06-27 18:02:51 +0800

    feat: add unit test

---
+++ languages/gvm/gvm_test.go
@@ -0,0 +1,27 @@
+package gvm
+
+import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGVM_ListRemoteVersions(t *testing.T) {
+	gvm := &GVM{}
+	remoteVersions, err := gvm.ListRemoteVersions(context.Background())
+	assert.NoError(t, err)
+	assert.NotNil(t, remoteVersions)
+}
+
+func TestGVM_ListInstalledVersions(t *testing.T) {
+	gvm := &GVM{}
+	installedVersions, err := gvm.ListInstalledVersions(context.Background())
+	assert.NoError(t, err)
+	assert.NotNil(t, installedVersions)
+}
+
+func TestGVM_GetDefaultVersion(t *testing.T) {
+	gvm := &GVM{}
+	defaultVersion := gvm.GetDefaultVersion(context.Background())
+	assert.NotNil(t, defaultVersion)
+}
